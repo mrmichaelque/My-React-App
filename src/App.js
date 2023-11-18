a72dbade8617660cbfc9b39d16c0bca7405718c8
First Last <mrmichaelque@gmail.com>
2023-11-18 03:15:20 -0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
+import React from 'react';
 import './App.css';
 import logo from './logo.svg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+  }
+
+  componentDidMount() {
+    this.callAPI();
+  }
+
+  callAPI() {
+    fetch("http://localhost:3000/testAPI")
+      .then((res) => res.text())
+      .then((res) => this.setState({ apiResponse: res }));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <p>{this.state.apiResponse}</p>
+      </div>
+    );
+  }
 }
 
 export default App;

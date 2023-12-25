fbe2a2a4de18fcb89e3e25a9f64b51f7b56d39b4
M C Q <129240033+mrmichaelque@users.noreply.github.com>
2023-12-25 12:41:04 -0800

    commit

--- launch.js
+++ launch.js
@@ -16,3 +16,65 @@
       }
     ]
   }
+  {
+    "configurations": [
+        {
+            "type": "pwa-msedge",
+            "name": "Launch Microsoft Edge",
+            "request": "launch",
+            "runtimeArgs": [
+                "--remote-debugging-port=9222"
+            ],
+            {
+                "propertyName": {
+                    // Add your property content here
+                }
+            }
+            "urls": [
+                  "c:\\Users\\Michael Que\\.vscode\\extensions\\ms-edgedevtools.vscode-edge-devtools-2.1.3\\out\\startpage\\app.js",
+                  "https://github.com/mrmichaelque/My-React-App/"
+                ],
+                "presentation": {
+                  "hidden": true
+                }
+              }
+        {
+            "type": "pwa-msedge",
+            "name": "Launch Microsoft Edge in headless mode",
+            "request": "launch",
+            "runtimeArgs": [
+                "--headless",
+                "--remote-debugging-port=9222"
+            ],
+            "url": "https://github.com/mrmichaelque/My-React-App/blob/9cdfb074b2ffee3ac936946627eafb62c7fa2293/App.js", // Provide your project's url to finish configuring
+            "presentation": {
+                "hidden": true
+            }
+        },
+        {
+            "type": "vscode-edge-devtools.debug",
+            "name": "Open Edge DevTools",
+            "request": "attach",
+            "url": "https://github.com/mrmichaelque/My-React-App/blob/9cdfb074b2ffee3ac936946627eafb62c7fa2293/App.js", // Provide your project's url to finish configuring
+            "presentation": {
+                "hidden": true
+            }
+        }
+    ],
+    "compounds": [
+        {
+            "name": "Launch Edge Headless and attach DevTools",
+            "configurations": [
+                "Launch Microsoft Edge in headless mode",
+                "Open Edge DevTools"
+            ]
+        },
+        {
+            "name": "Launch Edge and attach DevTools",
+            "configurations": [
+                "Launch Microsoft Edge",
+                "Open Edge DevTools"
+            ]
+        }
+    ]
+}

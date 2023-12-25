9a3221ff8709e615e9c5c9b7b563f53b1635c15d
M C Q <129240033+mrmichaelque@users.noreply.github.com>
2023-12-25 01:01:07 -0800

    commit

--- src/Pages/LandingPageStack.js
+++ src/Pages/LandingPageStack.js
@@ -1,51 +1,42 @@
-< !DOCTYPE html >
-    <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-                <title>Project Management Landing Page</title>
-                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/atom-design-system/2.0.0-beta.1/atom.min.css">
-                </head>
-                <body>
-                    <div class="container">
-                        <div class="hero">
-                            <h1 class="hero__title">Project Management Landing Page</h1>
-                            <p class="hero__subtitle">Here is some text to describe what we do.</p>
-                        </div>
-                        <div class="services">
-                            <h2 class="services__title">Our Services</h2>
-                            <div class="services__list">
-                                <div class="service">
-                                    <h3 class="service__title">Service 1</h3>
-                                    <p class="service__description">Here is some text to describe Service 1.</p>
-                                </div>
-                                <div class="service">
-                                    <h3 class="service__title">Service 2</h3>
-                                    <p class="service__description">Here is some text to describe Service 2.</p>
-                                </div>
-                                <div class="service">
-                                    <h3 class="service__title">Service 3</h3>
-                                    <p class="service__description">Here is some text to describe Service 3.</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="contact">
-                            <h2 class="contact__title">Contact Us</h2>
-                            <form class="contact__form">
-                                <div class="form__group">
-                                    <label for="name">Name</label>
-                                    <input type="text" id="name" name="name" placeholder="Name">
-                                </div>
-                                <div class="form__group">
-                                    <label for="email">Email</label>
-                                    <input type="email" id="email" name="email" placeholder="Email">
-                                </div>
-                                <div class="form__group">
-                                    <label for="message">Message</label>
-                                    <textarea id="message" name="message" placeholder="Message"></textarea>
-                                </div>
-                                <button class="btn" type="submit">Submit</button>
-                            </form>
-                        </div>
-                    </div>
-                </body>
-            </html>
+import logo from 'client/public'; // Import or define the path to your logo
+import Button from './components/Button.jsx'; // Corrected the import statement
+import './styles/App.scss'; // Adjust the path to your styles
+
+/**
+ * Renders the main App component.
+ * @returns {JSX.Element} The rendered App component.
+ */
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+
+      <Button /> {/* Render the Button component */}
+    </div>
+  );
+}
+
+// Correctly define the button function
+function Button() {
+  return (
+    <div className="button">
+      <button>My Profile</button>
+      <button>My Profile</button>
+    </div>
+  );
+}
+
+export default App;
--- src/functions/HStack.js
+++ src/functions/HStack.js
@@ -1,11 +1,11 @@
-function Feature({ title, desc, ...rest }) {
-    return (
-      <Box p={5} shadow='md' borderWidth='1px' {...rest}>
-        <Heading fontSize='xl'>{title}</Heading>
-        <Text mt={4}>{desc}</Text>
-      </Box>
-    )
-  }
+const Feature = function({ title, desc, ...rest }) {
+  return (
+    <Box p={5} shadow='md' borderWidth='1px' {...rest}>
+      <Heading fontSize='xl'>{title}</Heading>
+      <Text mt={4}>{desc}</Text>
+    </Box>
+  )
+};
   
   function StackEx() {
     return (
